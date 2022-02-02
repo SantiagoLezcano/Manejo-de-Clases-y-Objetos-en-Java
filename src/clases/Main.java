33a0b847b3c02e3fba6654aa158f34dc56cb33e4
SantiagoLezcano <89286837+SantiagoLezcano@users.noreply.github.com>
2022-02-02 16:10:21 -0500

    Organizacion de metodos main

--- src/clases/Main.java
+++ src/clases/Main.java
@@ -7,19 +7,29 @@ import java.text.SimpleDateFormat;
 
 public class Main {
     public static void main(String[] args) throws ParseException {
-        SimpleDateFormat dsf= new SimpleDateFormat("dd/MM/yyyy");
-        String dateAssitant= "26/11/1999";
+        personTest("26/11/1999");
+        fruitTest();
+        bankAccountTest();
+        animalTest();
+        computerTest();
+        bookTest();
+    }
+    /**
+     * Prueba de persona
+     */
 
-        /**
-         * Prueba de persona
-         */
+    public static void personTest(String dateAss) throws ParseException {
+        SimpleDateFormat dsf= new SimpleDateFormat("dd/MM/yyyy");
+        String dateAssitant=dateAss;
         Date dateBirth=dsf.parse(dateAssitant);
         System.out.println(" PERSON TEST");
-        Person person = new Person("1123","Santiago","Lezcano","Martínez",dateBirth,1);
+        Person person = new Person("1123","Santiago","Lezcano","Martínez",dateBirth,170);
         System.out.println(person.toString());
-        /**
-         * Prueba de Fruta
-         */
+    }
+    /**
+     * Prueba de Fruta
+     */
+    public static void fruitTest(){
         System.out.println("FRUIT TEST");
         ArrayList<String> colors;
         colors= new ArrayList<String>();
@@ -28,18 +38,20 @@ public class Main {
         colors.add("Yellow");
         Fruit fruit =new Fruit("Apple",13,colors);
         System.out.println(fruit.toString());
-
-        /**
-         * Prueba de Cuenta Bancaria
-         */
+    }
+    /**
+     * Prueba de Cuenta Bancaria
+     */
+    public static void bankAccountTest(){
         System.out.println("BANK ACCOUNT TEST");
         Bank_Account bank_account = new Bank_Account(2121321234,true);
         bank_account.statusAccount(bank_account.activated);
         System.out.println(bank_account.toString());
-
-        /**
-         * Prueba de Animal
-         */
+    }
+    /**
+     * Prueba de Animal
+     */
+    public static void animalTest(){
         System.out.println("ANIMAL TEST");
         ArrayList<String> typeAnimals;
         typeAnimals= new ArrayList<String>();
@@ -49,18 +61,19 @@ public class Main {
         typeAnimals.add("Mollusks");
         Animal animal= new Animal("Leon", (short) 4,"grande",typeAnimals,"Aggressive");
         System.out.println(animal.toString());
-
-        /**
-         * Prueba Computador
-         */
-
+    }
+    /**
+     * Prueba Computador
+     */
+    public static void computerTest(){
         System.out.println("COMPUTER TEST");
         Computer computer= new Computer(15,"Asus","123ASJ23","Ryzen 7","DDR4", (short) 16,false);
         System.out.println(computer.toString());
-
-        /**
-         * Prueba de Libro
-         */
+    }
+    /**
+     * Prueba de Libro
+     */
+    public static void bookTest(){
         System.out.println("BOOK TEST");
         Book book = new Book("Harry Potter","12AB23",(short)1300,"j.k. Rowling","Universal");
         System.out.println(book.toString());
